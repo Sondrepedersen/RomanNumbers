62c8e97a3e3fa9356952e78e312ae4756ed4e3cb
alper1999 <alper.adiyaman@hotmail.com>
2021-08-24 22:27:57 +0200

    grønn test2

--- src/RomanNumbers.java
+++ src/RomanNumbers.java
@@ -10,15 +10,38 @@ public class RomanNumbers {
     }
 
     private String toRoman(int i){
-        return "I";
+        String out;
+        if (i == 1){
+           out = "I";
+        } else if (i == 2){
+            out = "II";
+        } else if (i == 3){
+            out = "III";
+        } else {
+            out = null;
+        }
+        return out;
     }
+
+
+    private String toRomanLokke(int i){
+        String out = "";
+        for (int j = 0; j < i; j++) {
+            out = out + "I";
+
+        }
+           return out;
+        }
+
 
     @Test
     void shouldConvert2ToII (){
-        assertEquals("II" , toRomanII(1));
+        assertEquals("II" , toRoman(2));
     }
 
-    private String toRomanII(int i){
-        return "II";
+    @Test
+    void shouldConvert2ToIII (){
+        assertEquals("III" , toRomanLokke(3));
     }
+
 }
