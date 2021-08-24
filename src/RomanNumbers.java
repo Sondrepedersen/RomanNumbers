f9f1eff4b53d7bca0a698f040fd171defd70d7a9
Sondrepedersen <Sondreravndal@hotmail.com>
2021-08-24 20:40:04 +0200

    Initial commit

---
+++ src/RomanNumbers.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanNumbers {
+
+    @Test
+    void shouldConvert1ToI() {
+        assertEquals("I", toRoman(1));
+    }
+
+    private String toRoman(int i) {
+        return toRoman(1);
+    }
+
+
+}
